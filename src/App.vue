ccdb0269549e66e2dcefdf37458979bfdd67afba
5t <339792@whut.edu.cn>
2025-12-05 17:03:51 +0800

    Link Code Repo to GitHub repository

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <p class="affiliations">¹Wuhan University of Technology, Wuhan, China · ²Hubei University of Technology, Wuhan, China · tangxing@whut.edu.cn</p>
       <div class="links">
         <a href="https://arxiv.org/abs/2507.20128" target="_blank" rel="noopener" class="link">Paper</a>
-        <a href="#" class="link">Code Repo</a>
+        <a href="https://github.com/3328702107/smdim-music" target="_blank" rel="noopener" class="link">Code Repo</a>
       </div>
       <nav class="topnav">
         <a href="#accompaniment">1. Accompaniment</a>
